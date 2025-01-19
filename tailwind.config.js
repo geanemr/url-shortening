056996312d69d273c90de8d9b86847c9e0d1849a
geanemr <geaneramos1995@gmail.com>
2025-01-19 11:50:17 -0300

    style: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,31 @@ export default {
   theme: {
     extend: {
       colors: {
-        cyan
-      }
+        primary: {
+          cyan: "hsl(180, 66%, 49%)",
+          darkViolet: "hsl(257, 27%, 26%)",
+        },
+        secondary: {
+          red: "hsl(0, 87%, 67%)",
+        },
+        neutral: {
+          gray: "hsl(0, 0%, 75%)",
+          grayishViolet: "hsl(257, 7%, 63%)",
+          veryDarkBlue: "hsl(255, 11%, 22%)",
+          veryDarkViolet: "hsl(260, 8%, 14%)",
+        },
+      },
+      fontSize: {
+        body: "18px",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      fontWeight: {
+        medium: 500,
+        bold: 700,
+      },
     },
   },
   plugins: [],
-}
-
+};
