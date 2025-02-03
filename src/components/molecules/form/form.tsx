f03e1154fe36a785f07273dafa4e743b775c3139
geanemr <geaneramos1995@gmail.com>
2025-02-03 17:05:54 -0300

    chore: return sorted urls

--- src/components/molecules/form/form.tsx
+++ src/components/molecules/form/form.tsx
@@ -1,20 +1,21 @@
 import Input from "../../atoms/input/input";
 import Button from "../../atoms/button/button";
-import React from "react";
+import React, { useState } from "react";
 import { useFetch } from "../../../hooks/useFetch";
 import { useLocalStorage } from "../../../hooks/useLocalStorage";
+import { UrlItem, UrlList } from "../../../types/url";
 
 const Form = () => {
   const { fetchShortenUrl, url, setUrl, shortenUrl } = useFetch();
   const { setItem, getItem } = useLocalStorage();
+  const [showUrl, setShowUrl] = useState<UrlList>();
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
     fetchShortenUrl();
     if (!shortenUrl) return;
 
-    const storedUrls =
-      getItem<{ id: string; url: string }[]>("shortenUrls") || [];
+    const storedUrls = getItem<UrlList>("shortenUrls") || [];
     const lastId =
       storedUrls.length > 0 ? Number(storedUrls[storedUrls.length - 1].id) : 0;
     const newId = lastId + 1;
@@ -26,27 +27,42 @@ const Form = () => {
     const updatedUrls = [...storedUrls, newUrl];
 
     setItem("shortenUrls", updatedUrls);
+    const sortedUrls = updatedUrls.sort((a, b) => Number(b.id) - Number(a.id));
+    setShowUrl(sortedUrls as UrlList);
     setUrl("");
   };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="flex flex-wrap items-center m-2 lg:m-0 p-8 lg:p-12 bg-no-repeat bg-right-top bg-primary-darkViolet bg-inputMobile lg:bg-inputDesktop rounded-md gap-4 mb-6 mt-2"
-    >
-      <Input
-        placeholder="Shorten a link here..."
-        className="w-full md:w-auto truncate flex-grow p-3"
-        value={url}
-        onChange={(e) => setUrl(e.target.value)}
-      />
-      <Button
-        variant="square-cyan"
-        className="w-full md:w-auto truncate lg:break-normal flex-grow lg:flex-grow-0 p-3 lg:px-10"
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-wrap items-center m-2 lg:m-0 p-8 lg:p-12 bg-no-repeat bg-right-top bg-primary-darkViolet bg-inputMobile lg:bg-inputDesktop rounded-md gap-4 mb-6 mt-2"
       >
-        Shorten It!
-      </Button>
-    </form>
+        <Input
+          placeholder="Shorten a link here..."
+          className="w-full md:w-auto truncate flex-grow p-3"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <Button
+          variant="square-cyan"
+          className="w-full md:w-auto truncate lg:break-normal flex-grow lg:flex-grow-0 p-3 lg:px-10"
+        >
+          Shorten It!
+        </Button>
+      </form>
+      {showUrl ? (
+        <ul>
+          {showUrl.map((url: UrlItem) => (
+            <li key={url.id}>
+              <span>
+                {url.url} - {url.id}
+              </span>
+            </li>
+          ))}
+        </ul>
+      ) : null}
+    </>
   );
 };
 
