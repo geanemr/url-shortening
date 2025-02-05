6e6300b5bfcfc1ffad5f5d68e19eec89b12885b7
geanemr <geaneramos1995@gmail.com>
2025-02-04 22:57:26 -0300

    chore: mock handler simplification

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,10 @@
-import { http, HttpResponse } from 'msw';
+import { http, HttpResponse } from "msw";
 
 export const handlers = [
-  http.post('https://cleanuri.com/api/v1/shorten', async ({ request }) => {
+  http.post("https://cleanuri.com/api/v1/shorten", async ({ request }) => {
+    await request.json() as { url: string };
 
-    const { url }: { url: string } = await request.json() as { url: string };
-
-    const shortenedUrl = `https://mocked-url.com/${btoa(url)}`;
+    const shortenedUrl = `https://mocked-url.com/`;
 
     return HttpResponse.json({ result_url: shortenedUrl }, { status: 201 });
   }),
